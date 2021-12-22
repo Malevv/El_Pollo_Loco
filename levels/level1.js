abc11182fa9c13ac1771edb5603877073e4c2a48
Matthias Märsch <you@example.com>
2021-12-22 15:36:04 +0100

    endboss integrated

---
+++ levels/level1.js
@@ -0,0 +1,70 @@
+const level1 = new Level(
+
+    [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Endboss()
+    ],
+
+    [
+        new cloud()
+    ],
+
+    [      
+    
+        new BackgorundObjeckt('img/5.Fondo/Capas/5.cielo_1920-1080px.png', -719),
+        new BackgorundObjeckt('img/5.Fondo/Capas/3.Fondo3/2.png', -719),
+        new BackgorundObjeckt('img/5.Fondo/Capas/2.Fondo2/2.png', -719),
+        new BackgorundObjeckt('img/5.Fondo/Capas/1.suelo-fondo1/2.png', -719),
+
+        new BackgorundObjeckt('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 0),
+        new BackgorundObjeckt('img/5.Fondo/Capas/3.Fondo3/1.png', 0),
+        new BackgorundObjeckt('img/5.Fondo/Capas/2.Fondo2/1.png', 0),
+        new BackgorundObjeckt('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 0),
+
+        new BackgorundObjeckt('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719),
+        new BackgorundObjeckt('img/5.Fondo/Capas/3.Fondo3/2.png', 719),
+        new BackgorundObjeckt('img/5.Fondo/Capas/2.Fondo2/2.png', 719),
+        new BackgorundObjeckt('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719),
+
+        new BackgorundObjeckt('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719*2),
+        new BackgorundObjeckt('img/5.Fondo/Capas/3.Fondo3/1.png', 719*2),
+        new BackgorundObjeckt('img/5.Fondo/Capas/2.Fondo2/1.png', 719*2),
+        new BackgorundObjeckt('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 719*2),
+
+        new BackgorundObjeckt('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719*3),
+        new BackgorundObjeckt('img/5.Fondo/Capas/3.Fondo3/2.png', 719*3),
+        new BackgorundObjeckt('img/5.Fondo/Capas/2.Fondo2/2.png', 719*3),
+        new BackgorundObjeckt('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719*3),
+
+        new BackgorundObjeckt('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719*4),
+        new BackgorundObjeckt('img/5.Fondo/Capas/3.Fondo3/1.png', 719*4),
+        new BackgorundObjeckt('img/5.Fondo/Capas/2.Fondo2/1.png', 719*4),
+        new BackgorundObjeckt('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 719*4),
+
+        new BackgorundObjeckt('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719*5),
+        new BackgorundObjeckt('img/5.Fondo/Capas/3.Fondo3/2.png', 719*5),
+        new BackgorundObjeckt('img/5.Fondo/Capas/2.Fondo2/2.png', 719*5),
+        new BackgorundObjeckt('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719*5),
+
+        new BackgorundObjeckt('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719*6),
+        new BackgorundObjeckt('img/5.Fondo/Capas/3.Fondo3/1.png', 719*6),
+        new BackgorundObjeckt('img/5.Fondo/Capas/2.Fondo2/1.png', 719*6),
+        new BackgorundObjeckt('img/5.Fondo/Capas/1.suelo-fondo1/1.png', 719*6),
+
+        new BackgorundObjeckt('img/5.Fondo/Capas/5.cielo_1920-1080px.png', 719*7),
+        new BackgorundObjeckt('img/5.Fondo/Capas/3.Fondo3/2.png', 719*7),
+        new BackgorundObjeckt('img/5.Fondo/Capas/2.Fondo2/2.png', 719*7),
+        new BackgorundObjeckt('img/5.Fondo/Capas/1.suelo-fondo1/2.png', 719*7),
+    ]
+
+
+
+
+
+
+
+);
